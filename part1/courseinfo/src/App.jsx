c03202ddc49019e7d8502a9c63e5b0622714f179
ManueL20246 <pangopfrank@gmail.com>
2026-09-07 23:12:31 +0200

    exercice1.6...1.11 de unicafe

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,46 +1,83 @@
 import { useState } from "react"
 
-const Display = ({counter}) => <div>{counter}</div>
-/*{
+const Display = (props) => <h1>{props.name}</h1> 
+
+const StatisticLine = (props) => {
   return (
-    <div>{counter}</div>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
-}*/
-const Button = ({onSmash, text}) => <button onClick={onSmash}> {text} </button>
-/*{
+}
+
+const Statistics = (props) => {
+  if (props.value1 === 0 && props.value2 === 0 && props.value3 === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
-    <button onClick={onClick}>
+    <div>
+      <table border={0}>
+        <tbody>
+          <StatisticLine text="Good" value={props.value1} />
+          <StatisticLine text="Neutral" value={props.value2} />
+          <StatisticLine text="Bad" value={props.value3} />
+          <StatisticLine text="All" value={props.total} />
+          <StatisticLine text="Average" value={props.ave} />
+          <StatisticLine text="Positive" value={`${props.positive} %`}/>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  const {handleClick, text} = props
+  //console.log('props value is', props)
+  return (
+    <button onClick={handleClick}>
       {text}
     </button>
   )
-}*/
+}
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  /*setTimeout(
-    () => setCounter(counter + 1), 
-    1000 
-  )
-  const handleClick = () => {
-    console.log('cliked')
-  }*/
-  const increaseByOne =  () => setCounter(counter + 1)
-  const decreaseByOne =  () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const sum = good+neutral+bad
+  let avg = (good-bad)/sum
+  let pos = (good/sum)*100
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+  const handleAverage = () => {
+    if (good === 0 && neutral === 0 && bad === 0){
+      return avg = 0, pos = 0
+    }
+    return avg
+  }
+  const setToZero = () => {
+    setGood(0)
+    setNeutral(0)
+    setBad(0)
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne}
-        text = 'plus'
-      />
-      <Button onClick={decreaseByOne}
-        text = 'minus'
-      />
-      <Button onClick={setToZero}
-        text = 'Zero'
-      />
+      <Display name="Give feedback"/><br></br>
+      <Button handleClick={handleGoodClick} text="Good"/>&nbsp;
+      <Button handleClick={handleNeutralClick} text="Neutral"/>&nbsp;
+      <Button handleClick={handleBadClick} text="Bad"/>&nbsp;
+      <Button handleClick={setToZero} text="Reset"/><br></br>
+      <Display name="Statistics"/>
+      <Statistics value1={good} value2={neutral} value3={bad} total={sum} ave={handleAverage()} positive={pos}/>
     </div>
   )
 }
