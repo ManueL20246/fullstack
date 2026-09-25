6ff7f4050083d8abcdc74a574e2caf952b36d534
ManueL20246 <pangopfrank@gmail.com>
2026-09-25 23:16:57 +0200

    exercise_Unicafe&Anecdote_combined

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,6 +1,90 @@
 import { useState } from 'react'
 
-const App = () => {
+// --- Composant UNICAFE ---
+
+const Display = (props) => <h2>{props.name}</h2> 
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
+const Statistics = (props) => {
+  if (props.value1 === 0 && props.value2 === 0 && props.value3 === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <table border={0}>
+        <tbody>
+          <StatisticLine text="Good" value={props.value1} />
+          <StatisticLine text="Neutral" value={props.value2} />
+          <StatisticLine text="Bad" value={props.value3} />
+          <StatisticLine text="All" value={props.total} />
+          <StatisticLine text="Average" value={props.ave} />
+          <StatisticLine text="Positive" value={`${props.positive} %`}/>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+const Button = (props) => {
+  const {handleClick, text} = props
+  //console.log('props value is', props)
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const Unicafe = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const sum = good+neutral+bad
+  let avg = (good-bad)/sum
+  let pos = (good/sum)*100
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+  const handleAverage = () => {
+    if (good === 0 && neutral === 0 && bad === 0){
+      return avg = 0, pos = 0
+    }
+    return avg
+  }
+  const setToZero = () => {
+    setGood(0)
+    setNeutral(0)
+    setBad(0)
+  }
+
+  return (
+    <div>
+      <Display name="Give feedback"/>
+      <Button handleClick={handleGoodClick} text="Good"/>&nbsp;
+      <Button handleClick={handleNeutralClick} text="Neutral"/>&nbsp;
+      <Button handleClick={handleBadClick} text="Bad"/>&nbsp;
+      <Button handleClick={setToZero} text="Reset"/><br />
+      <Display name="Statistics"/>
+      <Statistics value1={good} value2={neutral} value3={bad} total={sum} ave={handleAverage()} positive={pos}/>
+    </div>
+  )
+}
+
+
+// --- Composant ANECDOTE ---
+
+const Anecdotes = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -28,14 +112,26 @@ const App = () => {
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
+      <h2>Anecdote of the day</h2>
       <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p><br></br>
+      <p>has {votes[selected]} votes</p>
       <button onClick={handleVoteClick}>vote</button>
-      <button onClick={handleAnecdoteClick}>next anecdote</button><br></br><br></br>
-      <h1>Anecdote with most votes</h1>
+      <button onClick={handleAnecdoteClick}>next anecdote</button><br/>
+      <h2>Anecdote with most votes</h2>
       <p>{anecdotes[votes.indexOf(Math.max(...votes))]}</p>
-      <p>{Math.max(...votes)}</p>
+      <p>has {Math.max(...votes)} votes</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Gestion Unicafe</h1>
+      <Unicafe />
+      <hr />
+      <h1>Gestion Anecdote</h1>
+      <Anecdotes />
     </div>
   )
 }
